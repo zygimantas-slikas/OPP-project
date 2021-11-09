80d903b731ecff755e6bd09d14cfc2e9f9b31a63
Žygimantas Šlikas <zygimantas.slikas1999@gmail.com>
2021-11-09 22:25:13 +0200

    comments

--- NUnit_tests/Server_room_test.cs
+++ NUnit_tests/Server_room_test.cs
@@ -1,4 +1,4 @@
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using Server;
 
 namespace NUnit_tests
@@ -8,13 +8,24 @@ namespace NUnit_tests
     {
         Room r1;
         /// <summary>
-        /// before every test
+        /// Parengimo metodas vykdomas prieš kiekvieną testavimo metodą
+        /// Sukuria naują Room klasės objektą kurį vėliau testuoja kiti metodai
         /// </summary>
         [SetUp]
         public void Setup()
         {
             r1 = new Room(1, 4, 10, 1, 1);
         }
+        /// <summary>
+        /// Testuojamas naujo Room objekto sukūrimas naudojant klasės knostruktorių
+        /// Tikrinama ar sukūrus naują objektą jo parametrai atitinka tuos kurie buvo paduoti metodui
+        /// Tikrinama ar nesukurta netinkamo tipo Item objektų 
+        /// </summary>
+        /// <param name="id">identifikacinis numeris</param>
+        /// <param name="players_count">maksimalus žaidėjų skaičius kurį prima objektas</param>
+        /// <param name="map_size">kuriamo žemėlapio dydis</param>
+        /// <param name="level">žaidimo lygis- žemėlapyje išmėtytų objektų spalva</param>
+        /// <param name="type">žemėlapio langelių tipų pasiskirstymo tipas</param>
         [Test]
         [TestCase(1, 4, 50, 1, 1)]
         [TestCase(1, 2, 20, 2, 2)]
@@ -59,6 +70,13 @@ namespace NUnit_tests
                 }
             }
         }
+        /// <summary>
+        /// Testas tikrina ar į žemėlapį pridedant naujus žaidėjus jų objektai padedami į 
+        /// tinkamas pozicijas. Kievienas žaidėjas žaidimą pradeda viename iš 4 kampų
+        /// pridėjus 4 žaidėjus visi kampai turėtų laikyti ne null objektą.
+        /// </summary>
+        /// <param name="con_id">prisijungusio žaidėjo identifikavimo kodas</param>
+        /// <param name="name">žaidėjo pasirinktas vardas</param>
         [Test]
         [TestCase("connection_id", "name")]
         public void Add_player_test(string con_id, string name)
@@ -74,6 +92,11 @@ namespace NUnit_tests
             Assert.That(r1.map[9, 0].Player_Standing, Is.Not.Null);
             Assert.That(r1.map[9, 9].Player_Standing, Is.Not.Null);
         }
+        /// <summary>
+        /// Testas tikirna ar pridėjus ir pašalinus žaidėją is Room objekto visi parametrai 
+        /// atstatomi taisingai. Kampiniai langeliai turi vėl neturėti žaidėjo - null, 
+        /// Room objekto žaidėjų sarašas turi laikyti 0 objektų.
+        /// </summary>
         [Test]
         public void Remove_player_test()
         {
@@ -83,6 +106,13 @@ namespace NUnit_tests
             Assert.AreEqual(r1.current_players, 0);
             Assert.AreEqual(r1.players.Count, 0);
         }
+        /// <summary>
+        /// Testuojamas žemėlapio serilizavimas į JSON formatą. Išserilizuojame tekste
+        /// neturėtų būti žaidėjų dauomenų lauko "Con_id" ir jo reikšmės, nes tai gali 
+        /// pakenkti saugumui (žaidėjai SignalR identifikuojami pagal Con_id todėl jis 
+        /// gali būti panaudotas prisijungimui pri kito žaidėjo paskyros)
+        /// Testas patikrina ar Regex šablonas neranda atitikimų JSON rezultate.
+        /// </summary>
         [Test]
         public void To_json_test()
         {
@@ -90,6 +120,10 @@ namespace NUnit_tests
             string json = r1.To_Json();
             Assert.That(json, Does.Not.Match("\"Con_id\":"));
         }
+        /// <summary>
+        /// Testuojamas žaidėjų sąršo serilizavimas i JSON formatą.
+        /// Testas patikrina ar Regex šablonas neranda atitikimų JSON rezultate.
+        /// </summary>
         [Test]
         public void Players_to_json_test()
         {
@@ -98,7 +132,7 @@ namespace NUnit_tests
             Assert.That(json, Does.Not.Match("\"Con_id\":"));
         }
         /// <summary>
-        /// affter every test
+        /// Resursų atlaisvinimas po kiekvieno testo
         /// </summary>
         [TearDown]
         public void CleanUp()
