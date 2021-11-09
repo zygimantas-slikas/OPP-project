a92f1d1fd5f360342bc669205b2a67330142b2c5
Karolis <61163709+Karolis@users.noreply.github.com>
2021-11-09 20:56:48 +0200

    t

---
+++ NUnit_tests/GameSettingsTests.cs
@@ -0,0 +1,25 @@
+﻿using Client;
+using NUnit.Framework;
+using System;
+
+namespace NUnit_tests
+{
+    [TestFixture]
+    public class GameSettingsTests
+    {
+        [Test]
+        public void SetSpeed_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var gameSettings = new GameSettings();
+            int newSpeed = 150;
+
+            // Act
+            gameSettings.SetSpeed(
+                newSpeed);
+
+            // Assert
+            Assert.AreEqual(gameSettings.PlayerDelaySpeed, 150);
+        }
+    }
+}
