627519443917a8d42a216ff9dd28bb542d2daea1
Žygimantas Šlikas <zygimantas.slikas1999@gmail.com>
2021-11-20 18:29:15 +0200

    analyser

---
+++ Analyzer_custom_rules/Testing_lab_3_Analyzer1/Testing_lab_3_Analyzer1.Test/Verifiers/VisualBasicCodeRefactoringVerifier`1+Test.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.CodeAnalysis.CodeRefactorings;
+using Microsoft.CodeAnalysis.Testing.Verifiers;
+using Microsoft.CodeAnalysis.VisualBasic.Testing;
+
+namespace Testing_lab_3_Analyzer1.Test
+{
+    public static partial class VisualBasicCodeRefactoringVerifier<TCodeRefactoring>
+        where TCodeRefactoring : CodeRefactoringProvider, new()
+    {
+        public class Test : VisualBasicCodeRefactoringTest<TCodeRefactoring, MSTestVerifier>
+        {
+        }
+    }
+}
